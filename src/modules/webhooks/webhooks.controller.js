ee97e3306ebcc2479b5cbc585721f0720c5db2b7
Jaime Corona <jaime_corona96@hotmail.com>
2023-10-12 23:18:27 -0600

    fix and console log body payload

--- src/modules/webhooks/webhooks.controller.js
+++ src/modules/webhooks/webhooks.controller.js
@@ -2,14 +2,14 @@ const { manageGithubWorkflow } = require("./webhooks.service");
 
 const webhookWorkflowController = (req, res) => {
   const body = req.body;
-
+  console.log(body);
   const properties = {
     repository: {
-      id: body.repository.id,
-      name: body.repository.name,
+      id: body?.repository?.id,
+      name: body?.repository?.name,
     },
-    action: body.action,
-    status: body.workflow_run.status,
+    action: body?.action,
+    status: body?.workflow_run?.status,
   };
 
   const response = manageGithubWorkflow(properties);
